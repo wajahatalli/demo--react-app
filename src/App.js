cca054ae91f04f61e8da96c5104e2890a4198271
wajahatalli <wajahat.jutt09@gmail.com>
2020-06-09 16:46:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <h1 align='center'><strong><em>Offer valid till 12 a.m</em></strong></h1>
       
       <hr />
-      <p align='center'><strong>Are you guys ready?</strong></p>
+      <p align='center'><strong>Are you guys ready??</strong></p>
     </div>
   );
 }
